0da38b6b9ecf6c87c9323d309add99d2c267b101
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-17 23:43:59 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+main.addEventListener('click',function (event) { 
+  alert('I was clicked!');
+});
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function (e) {
+
+  if(e.which === 65){
+    console.log("default is prevented");
+    return e.preventDefault();
+  } else {
+    console.log ("not an a")
+  }
+})
+
+let divs = document.querySelectorAll("div");
+
+function bubble(e) {
+  e.stopPropagation();
+  console.log(this.firstChild.nodeValue.trim() + "bubbled");
+}
+
+for (let i = 0 ; i < divs.length; i++) {
+  divs[i].addEventListener("click", bubble);
+}
+
+function capture(e) {
+
+  console.log(this.firstChild.nodeValue.trim() + "caputred");
+}
+
+for (let i = 0 ; i < divs.length; i++) {
+  divs[i].addEventListener("click",capture,true);
+} 
